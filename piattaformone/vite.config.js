5acd2d871457a7ecabd6f8067614c45895bde54e
AleD <alessandrodaniellodev@gmail.com>
2026-01-19 17:45:36 +0100

    [test]: vite fix (not working)

--- piattaformone/vite.config.js
+++ piattaformone/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            detectTls: false, // Disabilita il rilevamento TLS
         }),
         vue({
             template: {
@@ -20,6 +21,13 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        host: '0.0.0.0',
+        port: 5174,
+        strictPort: false,
+        hmr: {
+            host: process.env.LARAVEL_SAIL ? 'host.docker.internal' : 'localhost',
+            port: 5174,
+        },
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
