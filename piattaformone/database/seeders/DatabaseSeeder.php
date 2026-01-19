8876ce7c445d2056d879c00575d271ceffe9dbd7
AleD <alessandrodaniellodev@gmail.com>
2026-01-19 15:31:13 +0100

    [add]: jetstream + inertia (vue)

--- piattaformone/database/seeders/DatabaseSeeder.php
+++ piattaformone/database/seeders/DatabaseSeeder.php
@@ -3,31 +3,28 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // User::factory(10)->withPersonalTeam()->create();
 
-        User::factory()->create([
+        User::factory()->withPersonalTeam()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
 
         // Create admin user
-        User::factory()->create([
+        User::factory()->withPersonalTeam()->create([
             'name' => 'Alessandro D\'Aniello',
             'email' => 'alessandrodaniellodev@gmail.com',
-            'password' => Hash::make('password'),
+            'password' => 'password',
         ]);
     }
 }
