2a333bf8da382ef07412c468fe3b56607beef94d
AleD <alessandrodaniellodev@gmail.com>
2026-01-19 15:16:43 +0100

    [add]: 1 admin user seeder

--- piattaformone/database/seeders/DatabaseSeeder.php
+++ piattaformone/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +21,13 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        // Create admin user
+        User::factory()->create([
+            'name' => 'Alessandro D\'Aniello',
+            'email' => 'alessandrodaniellodev@gmail.com',
+            'password' => Hash::make('password'),
         ]);
     }
 }
